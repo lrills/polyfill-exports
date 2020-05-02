a888d1f263d01f5485fd479752d0d0dc092b52ff
Patrick Liu <zeldalink0515@gmail.com>
2020-05-02 17:56:23 +0800

    Enhance cli logging

--- cli.js
+++ cli.js
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
+import { resolve as resolvePath } from 'path'
 import polyfill from './index.js'
 
 const [, , packagePath] = process.argv
-const results = polyfill(packagePath)
+const absolutePkgPath = resolvePath(packagePath)
+const results = polyfill(absolutePkgPath)
 
-console.log(`\nLinking files to export subpaths are created:\n`)
+if (results.length > 0) {
+  console.log(`\nSubpath linking files are created under ${absolutePkgPath}:\n`)
 
-const maxEntryNameLen = results.reduce(
-  (maximum, [entryName]) => Math.max(maximum, entryName.length),
-  0
-)
-
-for(const [entryFile, targetPath] of results) {
-  const spacesForAlignment = ' '.repeat(maxEntryNameLen - entryFile.length)
-  console.log(
-    `\t${entryFile}${spacesForAlignment}  ->  ${targetPath}`
+  const maxEntryNameLen = results.reduce(
+    (maximum, [entryName]) => Math.max(maximum, entryName.length),
+    0
   )
-}
 
-console.log(`\nplease make sure they are added to the VCS.\n`)
+  for(const [entryFile, targetPath] of results) {
+    const spacesForAlignment = ' '.repeat(maxEntryNameLen - entryFile.length)
+    console.log(
+      `\t${entryFile}${spacesForAlignment}  ->  ${targetPath}`
+    )
+  }
+
+  console.log(`\nplease make sure they are added to the VCS.\n`)
+} else {
+  console.log(`No subpath exports under ${absolutePkgPath}`)
+}
